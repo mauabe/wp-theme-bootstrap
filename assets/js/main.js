0df0bffc827c7416cfe0aa6acf56903853d1e18e
Mauricio <mauabe@gmx.com>
2019-05-19 23:06:14 -0700

    downgraded font-awesome

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,37 @@ $(function(){
     });
   });
 });
+
+/*
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
+
+
+const modalBtn = document.querySelector('button[data-toggle="modal"]');
+
+const modal = document.querySelector('.modal');
+console.log('modal div',  modal);
+
+const showModal = function() {
+  // console.log('you clicked the button'); //works
+  // console.log('modalButton', modalBtn);  //works
+    modal.classList.add('in', 'show', 'modal-open');
+};
+// const hideModal = function(){
+//   let popup = document.getElementById('modal');
+//   popup.classList.remove('show');
+// };
+
+
+$(document).ready(function(){
+  // console.log('jquery here');
+  $("modalBtn").click(function(){
+    $("#myModal").modal();
+  });
+});
+
+modalBtn.addEventListener('click', showModal)
+*/
+
